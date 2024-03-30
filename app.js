56f948f7f877976575cbe38cd02a86732ec9804f
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-03-30 01:53:59 -0400

    patch and delete routes added

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const error = require("./utilities/error");
 
 var path = require("path");
 
-// Importing the data from database files.
+// Importing the data from database files.===========
 const usersRoutes = require("./routes/usersRoutes");
 const postsRoutes = require("./routes/postsRoutes");
 const commentsRoutes = require("./routes/commentsRoutes");
 
-// Custom logging requests middleware.
+// Custom logging requests middleware.==============
 const logReq = function (req, res, next) {
     console.log("Request Received");
     next();
@@ -22,21 +22,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(logReq);
 
-// Use our Routes
+// Use our Routes===========
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/comments", commentsRoutes)
 
-// rendering views with ejs
+// rendering views with ejs==============
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "styles")));
 
-//////////////GET ROUTE TO RENDER THE FORM 
+//////////////GET ROUTE TO RENDER THE FORM ===========
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-// POST ROUTE TO HANDLE THE FORM SUBMISSION
+// POST ROUTE TO HANDLE THE FORM SUBMISSION============
 
 app.post("/submit", (req, res) => {
     const { name, username, email } = req.body;
@@ -45,14 +45,27 @@ app.post("/submit", (req, res) => {
     res.send("Form submitted successfully!");
 });
 
+//// PATCH route to update user data==============
+app.patch("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    // Update user data based on userId
+    res.send(`Updated user with id ${userId}`);
+});
 
-// Custom 404 (not found) middleware.
+///// DELETE route to delete user data==============
+app.delete("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    // Delete user data based on userId
+    res.send(`Deleted user with id ${userId}`);
+});
+
+// Custom 404 (not found) middleware=========
 app.use((req, res) => {
     res.status(404);
     res.json({ error: "Resource Not Found" });
 });
 
-// Error-handling middleware.
+// Error-handling middleware.=============
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({ error: err.message });
